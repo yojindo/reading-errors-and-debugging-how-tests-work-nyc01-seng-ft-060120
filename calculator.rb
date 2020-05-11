6618fce01f50786e952970438c0a8317389fa624
Jake Kwak <jakejaekwak@gmail.com>
2020-05-11 17:29:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@
 first_number = 1.0
 second_number = 1
 sum = first_number + second_number
-difference = first_number + second_number
+difference = first_number - second_number
 product = first_number * second_number
